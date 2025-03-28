9bfc6db14d6cae233c02db01df363ad642aefa69
basybemoog <st135522@student.spbu.ru>
2025-03-28 16:04:12 +0300

    clear cache from .idea and cmake build debug

--- cmake-build-debug/CMakeFiles/lab_2_game.dir/CMakeFiles/3.30.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/lab_2_game.dir/CMakeFiles/3.30.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj: \
- C:\Users\basybemoog\CLionProjects\lab_2_game\cmake-build-debug\CMakeFiles\3.30.5\CompilerIdCXX\CMakeCXXCompilerId.cpp
--- cmake-build-debug/CMakeFiles/lab_2_game.dir/build/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/lab_2_game.dir/build/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp.obj: \
- C:\Users\basybemoog\CLionProjects\lab_2_game\build\CMakeFiles\3.31.6\CompilerIdCXX\CMakeCXXCompilerId.cpp
