302a7fb8e5b42b7ddfe8078fd0cfee80bd303676
basybemoog <st135522@student.spbu.ru>
2025-04-18 20:01:22 +0300

    some updates in shop

--- src/characters/main_char.h
+++ src/characters/main_char.h
@@ -3,7 +3,7 @@
 
 #include <memory>
 #include <unordered_map>
-#include "iostream"
+#include <iostream>
 #include "character.h"
 #include "../inventory/coins/coins.h"
 #include "../utils/slot_of_equipment.h"
@@ -11,6 +11,7 @@
 #include "../inventory/weapon/weapon.h"
 #include "../inventory/bag/bag.h"
 #include "../inventory/inventory.h"
+
 
 class main_char : public character {
 public:
