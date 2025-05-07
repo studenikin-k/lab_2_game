22b46392b9a8c9d82e059952aef93a39b33668dd
basybemoog <st135522@student.spbu.ru>
2025-05-07 23:39:38 +0300

    remove usless methods from bag.cpp

--- src/inventory/bag/bag.cpp
+++ src/inventory/bag/bag.cpp
@@ -2,25 +2,19 @@
 #include <algorithm>
 #include <vector>
 #include <limits>
+#include "../../characters/main_char/main_char.h"
 
 
-void bag::inputIntoBag(equipment* item) {
-
-
+void bag::inputIntoBag(equipment *item) {
     bagEquipment.push_back(item);
-
 }
 
-void bag::inputIntoBag(weapon* Gun) {
-
+void bag::inputIntoBag(weapon *Gun) {
     bagWeapon.push_back(Gun);
-
 }
 
-void bag::inputIntoBag(potion* Potion) {
-
+void bag::inputIntoBag(potion *Potion) {
     bagPotion.push_back(Potion);
-
 }
 
 void bag::outputEquipmentFromBag(const std::string &_name) {
@@ -35,38 +29,10 @@ void bag::outputWeaponFromBag(const std::string &_name) {
     });
 }
 
-    void bag::outputPotionFromBag(const std::string &_name) {
-        std::erase_if(bagPotion, [&_name](const auto &item) {
-            return item->getName() == _name;
-        });
-    }
-
-void bag::showBag() const {
-    std::cout << "Что показать?\n"
-              << "  1) Снаряжение\n"
-              << "  2) Оружие\n"
-              << "  3) Зелья\n"
-              << "Ваш выбор: ";
-
-    int choice;
-    if (!(std::cin >> choice)) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Некорректный ввод.\n";
-        return;
-    }
-
-    switch (choice) {
-        case 1:
-            printCategory(bagEquipment, "Снаряжение");
-        break;
-        case 2:
-            printCategory(bagWeapon, "Оружие");
-        break;
-        case 3:
-            printCategory(bagPotion, "Зелья");
-        break;
-        default:
-            std::cout << "Неправильный выбор.\n";
-    }
+void bag::outputPotionFromBag(const std::string &_name) {
+    std::erase_if(bagPotion, [&_name](const auto &item) {
+        return item->getName() == _name;
+    });
 }
+
+
--- src/inventory/bag/bag.h
+++ src/inventory/bag/bag.h
@@ -24,26 +24,12 @@ public:
 
     void outputPotionFromBag(const std::string &name);
 
-    void showBag() const;
+    void showBag() ;
 
     std::vector<equipment *> bagEquipment;
     std::vector<weapon *> bagWeapon;
     std::vector<potion *> bagPotion;
-private:
 
-    template<typename T>
-    void printCategory(const std::vector<T*>& vec, const std::string& title) const {
-        std::cout << "\n=== " << title << " ===\n";
-        if (vec.empty()) {
-            std::cout << "  (пусто)\n\n";
-            return;
-        }
-        for (size_t i = 0; i < vec.size(); ++i) {
-            std::cout << "  " << (i + 1) << ") ";
-            vec[i]->showInInventory();
-        }
-        std::cout << "\n";
-    }
 };
 
 
